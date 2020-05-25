758ed24dc87748d22956be53f282b98852540a15
vladstojna <vlad.leic@gmail.com>
2020-05-25 11:44:59 +0100

    delete unused classes

--- src/pt/ulisboa/tecnico/cnv/load_balancer/callback/IEvaluationCallback.java
+++
@@ -1,8 +0,0 @@
-package pt.ulisboa.tecnico.cnv.load_balancer.callback;
-
-import pt.ulisboa.tecnico.cnv.load_balancer.instance.WorkerInstanceHolder;
-
-public interface IEvaluationCallback {
-	void createInstance(WorkerInstanceHolder createdInstance);
-	void terminateInstance(WorkerInstanceHolder terminatedInstance);
-}
